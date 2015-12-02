787be41a56199a31ef6b1e1776750a06c9bf1d55
louis_bluhm <Louis@DeltaApsire.campus.goldsmiths.ac.uk>
2015-12-02 11:05:01 +0000

    Attempted index.html and index.js bug fix

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function(req, res) {
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
