8e66b045d255d41add4596731c6e2e73f17ddc92
louis_bluhm <Louis@DeltaApsire.campus.goldsmiths.ac.uk>
2015-12-02 11:38:23 +0000

    Fixed index.js error, added chat message function

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 	console.log('user connected');
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + msg);
+	});
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 });
 
 http.listen(3000, function() {
